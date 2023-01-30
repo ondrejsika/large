a9b1968ef81759da4af4f512af9dbf3365de05e9
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-30 22:55:05 +0100

    refactor: Move command logic to cmd package

--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	_ "github.com/ondrejsika/training-cli/cmd"
-	training_cli_root "github.com/ondrejsika/training-cli/cmd/root"
-	_ "github.com/sikalabs/signpost/cmd"
-	signpost_root "github.com/sikalabs/signpost/cmd/root"
-	_ "github.com/sikalabs/slu/cmd"
-	slu_root "github.com/sikalabs/slu/cmd/root"
-	"github.com/spf13/cobra"
-)
-
-var LargeCmd = &cobra.Command{
-	Use:   "large",
-	Short: "slu, signpost & training-cli in large binary",
-}
-
-func init() {
-	LargeCmd.AddCommand(slu_root.RootCmd)
-	LargeCmd.AddCommand(training_cli_root.Cmd)
-	LargeCmd.AddCommand(signpost_root.Cmd)
-}
+import "github.com/ondrejsika/large/cmd"
 
 func main() {
-	LargeCmd.Execute()
+	cmd.Execute()
 }
